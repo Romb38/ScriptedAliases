0f18755b34deeb6ae5730619251ac45b1bd07e55
Romb38 <r.barbier38@gmail.com>
2025-04-07 15:32:13 +0200

    Adding list commands

---
+++ commandList.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#alias="ls"
+
+# Récupérer le chemin du dossier où se trouve le script
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# Lire le fichier commands.txt situé dans le même dossier que le script
+cat "$SCRIPT_DIR/commands.txt"
