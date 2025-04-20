05fd4ca44682989888ca4796ef9cd43187a67068
Romb38 <r.barbier38@gmail.com>
2025-04-20 20:15:55 +0200

    Translation of run_aliase.sh Update source.sh to add sudo show it on verbosity level 2

--- run_alias.sh
+++ run_alias.sh
@@ -2,41 +2,42 @@
 
 #MS_IGNORE
 
-# Récupérer le chemin absolu du répertoire où se trouve le script
+# Root script folder
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
-# Vérifier si l'alias est fourni en tant qu'argument
+# Check if aliases if given as args
 if [ $# -lt 1 ]; then
     echo "Usage: ms <alias> [<param1> <param2> ...]"
     exit 1
 fi
 
 alias="$1"
-shift  # Supprimer le premier argument (l'alias) de la liste des arguments
+shift  # Removing $1 from args
 
-# Vérifier si le fichier aliases.txt existe
+# Check if aliases list file exists
 if [ ! -f "$SCRIPT_DIR/aliases.txt" ]; then
-    echo "Le fichier aliases.txt n'existe pas."
+    echo "Aliases list doesn't exists. Please check installation"
     exit 1
 fi
 
-# Récupérer le chemin du fichier correspondant à l'alias
+# Get alias source file path
 file_path=$(grep "^$alias=" "$SCRIPT_DIR/aliases.txt" | cut -d '=' -f 2)
 
-# Vérifier si l'alias est trouvé
+# If alias is not found
 if [ -z "$file_path" ]; then
-    echo "Alias '$alias' non trouvé dans aliases.txt."
+    echo "Alias '$alias' not found in aliases list."
     exit 1
 fi
 
-# Vérifier si le fichier correspondant existe
+# Check if alias source file not found
 if [ ! -f "$file_path" ]; then
-    echo "Fichier correspondant à l'alias '$alias' ($file_path) non trouvé."
+    echo "File coressponding to '$alias' ($file_path) not found."
     exit 1
 fi
 
-# Vérifier si le fichier contient #isSudo
-if grep -q "^#isSudo" "$file_path"; then
+# Check if the file need to be executed as sudo
+# To do so, we check if the alias source file contains #MS_SUDO
+if grep -q "^#MS_SUDO" "$file_path"; then
     sudo bash "$file_path" "$@"
 else
     bash "$file_path" "$@"
--- source.sh
+++ source.sh
@@ -3,8 +3,6 @@
 #MS_ALIAS="source"
 #MS_SYSTEM
 
-
-# Verbosity level detection
 verbosity=0
 case "$1" in
   -v|--verbose) verbosity=1 ;;
@@ -12,31 +10,27 @@ case "$1" in
   -vvv|--vvverbose) verbosity=3 ;;
 esac
 
-# Get absolute path to script file
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 SCRIPTS_LIST_FILE="$SCRIPT_DIR/scripts_directory.txt"
 
-# If aliases repository lists file doesn't exists, we create it with the ScriptedAliases root folder
 if [ ! -f "$SCRIPTS_LIST_FILE" ]; then
   echo "Aliases repository list file does not exist. Generating default..."
   echo "$SCRIPT_DIR" > "$SCRIPTS_LIST_FILE"
   echo "File generated in $SCRIPT_DIR"
 fi
 
-# Initialisation
 alias_count=0
 system_alias_count=0
 no_alias_files=()
 conflict_map=()
 declare -A alias_to_file
 declare -A alias_is_system
+declare -A alias_need_sudo
 shopt -s nullglob
 
-# Reset outputed files
 echo "# Aliases fetched from .sh files founded in aliases repository" > "$SCRIPT_DIR/aliases.txt"
 echo "" > "$SCRIPT_DIR/commands.txt"
 
-# Treatement of given directory
 process_directory() {
   local dir="$1"
   local files=("$dir"/*.sh)
@@ -44,12 +38,10 @@ process_directory() {
   chmod u+x "${files[@]}" 2>/dev/null
 
   for file in "${files[@]}"; do
-    # Ignore file with #MS_IGNORE
     if grep -q '^#MS_IGNORE' "$file"; then
       continue
     fi
 
-    # Check aliases list
     alias_line=$(grep -m 1 '^#MS_ALIAS=' "$file")
     if [[ -n $alias_line ]]; then
       alias_name=$(echo "$alias_line" | sed 's/^#MS_ALIAS=//;s/"//g')
@@ -58,6 +50,10 @@ process_directory() {
       if grep -q '^#MS_SYSTEM' "$file"; then
         is_system=true
         alias_is_system["$alias_name"]=true
+      fi
+
+      if grep -q '^#MS_SUDO' "$file"; then
+        alias_need_sudo["$alias_name"]=true
       fi
 
       if [[ -n "${alias_to_file[$alias_name]}" ]]; then
@@ -76,14 +72,12 @@ process_directory() {
   done
 }
 
-# List aliases repository from scripts_directory.txt
 while IFS= read -r dir; do
   [[ -z "$dir" || "$dir" =~ ^# ]] && continue
   dir="${dir/#\~/$HOME}"
   process_directory "$dir"
 done < "$SCRIPTS_LIST_FILE"
 
-# Save valid aliases
 for alias_name in "${!alias_to_file[@]}"; do
   file="${alias_to_file[$alias_name]}"
   echo "$alias_name=$file" >> "$SCRIPT_DIR/aliases.txt"
@@ -96,12 +90,10 @@ for alias_name in "${!alias_to_file[@]}"; do
     ((alias_count++))
   fi
 
-  # Display infos coressponding to verbosity level
   if (( verbosity >= 2 )); then
     display_name="$alias_name"
-    if [[ "${alias_is_system[$alias_name]}" == "true" ]]; then
-      display_name+=" (system)"
-    fi
+    [[ "${alias_is_system[$alias_name]}" == "true" ]] && display_name+=" (system)"
+    [[ "${alias_need_sudo[$alias_name]}" == "true" ]] && display_name+=" (sudo)"
 
     if (( verbosity == 2 )); then
       echo "🔹 $display_name"
@@ -111,7 +103,6 @@ for alias_name in "${!alias_to_file[@]}"; do
   fi
 done
 
-# Summary
 echo "Finished."
 if (( verbosity >= 1 )); then
   echo "$system_alias_count system alias(es) founded"
